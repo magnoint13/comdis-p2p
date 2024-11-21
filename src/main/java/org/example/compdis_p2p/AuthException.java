4f9c9b2dde56e1b300318d310e867eba933c4fdd
MarcosG <marcosgranja317@gmail.com>
2024-11-22 00:06:22 +0100

    Añadir excepciones básicas requeridas por las interfaces

---
+++ src/main/java/org/example/compdis_p2p/AuthException.java
@@ -0,0 +1,7 @@
+package org.example.compdis_p2p;
+
+public class AuthException extends RuntimeException {
+    public AuthException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/example/compdis_p2p/NotOnlineException.java
@@ -0,0 +1,7 @@
+package org.example.compdis_p2p;
+
+public class NotOnlineException extends Exception {
+    public NotOnlineException(String message) {
+        super(message);
+    }
+}
